e6a486be931d0e16109ee7f833ff23689b3e7150
mack <mhyman@bu.edu>
2025-11-14 12:59:37 -0500

    revised mean WM optical props to measure the median instead

--- analyses/mean_wm_optical_props.m
+++ analyses/mean_wm_optical_props.m
@@ -24,18 +24,13 @@ clc; close all;
 % Add top-level directory + subdirectories
 addpath(genpath(fullfile(pwd, '..')))
 % Directory for loading seg, mus, ret, mask, epvs
-data_dir = ['/autofs/cluster/octdata3/users/mjhyman/' ...
-    'oct_caa_analyses/optical_properties'];
-% WM mask directory (from Taylor)
-wm_dir = ['/autofs/space/turtle_001/users/xz875/projects/' ...
-          'Multi-resolution_Unets_Semi_OCT/prediction'];
-% Voxel dimensions (microns) for all runs
-res = [20,20,20]; % resolution in microns
-
-%%% flag for reloading the .MAT struct for each subject
+data_dir = '/projectnb/npbssmic/ns/CAA/';
+% flag for reloading the .MAT struct for each subject
 flag_load_caa_structs = false;
 
-
+%% Set the upper limits for mus and retardance
+mus_max = 25;
+ret_max = 45;
 
 %% Load each subject's .MAT struct and create WM mask
 if flag_load_caa_structs
@@ -78,7 +73,8 @@ subjects.caa26 = caa26;
 
 % retrieve subject IDs
 subs = fieldnames(subjects);
-parench = struct();
+% Initialize struct
+parench_median = struct();
 
 % Loop over each subject
 for i = 1:length(subs)
@@ -88,15 +84,17 @@ for i = 1:length(subs)
     regions = fieldnames(subjects.(sub));
     % Loop over each region (e.g., 'front', 'occip')
     for j = 1:length(regions)
+        % Print to console
+        fprintf('Starting subject %s region %s.\n',sub,reg)
         % Region (front or occip)
-        loc = regions{j};  
+        reg = regions{j};  
         % Load the local parameters dynamically
-        [mus, ret, seg, seg_wm, ori, epvs] = ...
-            load_local_params(subjects.(sub), loc);
+        [mus, ret, seg, seg_wm, ~, epvs] = ...
+            load_local_params(subjects.(sub), reg);
         
         %%% Apply white matter mask
         % Retrieve white matter mask
-        wm = subjects.(sub).(loc).mask_wm;
+        wm = subjects.(sub).(reg).mask_wm;
         % Set tissue outside WM mask to NaN
         mus(~wm) = NaN;
         ret(~wm) = NaN;
@@ -105,26 +103,40 @@ for i = 1:length(subs)
         % measurements are only of parenchyma
         mus(seg) = NaN;
         ret(seg) = NaN;
-        ori(seg) = NaN;
         if ~isempty(epvs)
             epvs = logical(epvs);
             mus(epvs) = NaN;
             ret(epvs) = NaN;
-            ori(epvs) = NaN;
         end
+        
+        %%% Omit values above threshold then apply 1.5*IQR removal
+        mus = omit_outlier(mus,mus_max);
+        ret = omit_outlier(ret,ret_max);       
 
-        %%% Measure average mus, retardance, orientation
-        mus = mean(mus,'omitnan');
-        ret = mean(ret,'omitnan');
-        % Convert orientation to radians
-        ori = rmmissing(deg2rad(ori(wm)));
-        ori_std = circ_std(ori(:));
+        %%% Median mus, retardance, orientation
+        mus_median = median(mus, 'omitnan');
+        ret_median = median(ret, 'omitnan');
 
         %%% Save to struct
-        fprintf('Finished subject %s region %s.\n',sub,loc)
-        parench.(sub).(loc).mus = mus;
-        parench.(sub).(loc).ret = ret;
-        parench.(sub).(loc).ori = ori;
-
+        fprintf('Finished subject %s region %s.\n',sub,reg)
+        % Medians
+        parench_median.(sub).(reg).med.mus = mus_median;
+        parench_median.(sub).(reg).med.ret = ret_median;
     end
 end
+
+% Clear subjects to reduce memory usage
+clear subjects mus ret seg epvs
+
+%% Save to folder
+
+% Retrieve current date
+d = string(datetime('now','TimeZone','local','Format','d-MMM-y'));
+% Write filename
+fout = string("median_white_matter_values_" + d + '.mat');
+% Create output filepath
+fout = fullfile(data_dir,fout);
+% Save output
+save(fout, 'parench_median');
+% Print to console that job is done
+fprintf('Finished saving median values to %s.\n', fout);
