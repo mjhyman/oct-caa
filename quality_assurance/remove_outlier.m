01f05ab42546adc0bcc5ac69ed79395d6c23c34b
mack <mhyman@bu.edu>
2025-12-01 16:47:11 -0500

    revised outlier removal. output new matlab struct 'parenchyma_optical_properties_40um_thick_03Nov2025.mat'

--- quality_assurance/remove_outlier.m
+++ quality_assurance/remove_outlier.m
@@ -13,13 +13,14 @@ The full volumes are here:
 
 Outline:
 - IMPORT struct containing:
-    - tissue masks (exclude background signal)
-    - EPVS
-    - segmented vasculature (from Etienne)
-    - optical properties (mus + retardance)
+    - optical property donut measurements at each radii
+    - inner and outter
+    - each radius
 - REMOVE outliers
     - mus range = [0,25]
     - retardance range = [0,45]
+- Save as .MAT with latest date (03Nov2025) to match the spreadsheet
+    "caa_all_radii_40um_donut_03Nov2025.xlsx"
 %}
 
 %% Prepare environment
@@ -28,46 +29,76 @@ clear; clc; close all;
 addpath(genpath(fullfile(pwd, '..')))
 % Directory for loading seg, mus, ret, mask, epvs
 data_dir = '/projectnb/npbssmic/ns/CAA/';
-% Output figure directory
-fig_out = '/projectnb/npbssmic/ns/CAA/bw_plots/';
-% Stats output directory
-stats_out = '/projectnb/npbssmic/ns/CAA/metrics/';
 % Voxel dimensions (microns) for all runs
 res = [20,20,20]; % resolution in microns
 
-%%% Flag for loading .MAT struct and creating WM masks
-% flag for reloading the .MAT struct for each subject
-flag_load_caa_structs = true;
+% Import the spreadsheet
+fname = 'parenchyma_optical_properties_40um_thick_09Oct2025.mat';
+parench = load(fullfile(data_dir, fname));
+parench = parench.parench;
 
-% down sampling factor for the boxplot arrays
-ds = 4;
+% Output .MAT name
+fout = 'parenchyma_optical_properties_40um_thick_03Nov2025.mat';
 
-%% Load each subject's .MAT struct and create WM mask
-%%% Load the .MAT structs
-if flag_load_caa_structs
-    % CAA 6
-    fprintf('Loading CAA6\n')
-    caa6 = load(fullfile(data_dir,'/caa6/caa6.mat'));
-    caa6 = caa6.caa6;
-    fprintf('Finished Loading CAA6\n')
-    % CAA 17
-    fprintf('Loading CAA17\n')
-    caa17 = load(fullfile(data_dir,'/caa17/occip/caa17.mat'));
-    caa17 = caa17.caa17;
-    fprintf('Finished Loading CAA17\n')
-    % CAA 22
-    fprintf('Loading CAA22\n')
-    caa22 = load(fullfile(data_dir,'/caa22/caa22.mat'));
-    caa22 = caa22.caa22;
-    fprintf('Finished Loading CAA22\n')
-    % CAA 25
-    fprintf('Loading CAA25\n')
-    caa25 = load(fullfile(data_dir,'/caa25/caa25.mat'));
-    caa25 = caa25.caa25;
-    fprintf('Finished Loading CAA25\n')
-    % CAA 26
-    fprintf('Loading CAA26\n')
-    caa26 = load(fullfile(data_dir,'/caa26/caa26.mat'));
-    caa26 = caa26.caa26;
-    fprintf('Finished Loading CAA26\n')
+% Outlier threshold
+max_pmus = 25;
+max_pret = 45;
+
+%% Remove outliers from mus and retardance
+
+% Iterate subjects
+subs = fieldnames(parench);
+for ii = 1:numel(subs)
+    % Iterate regions (front and occip)
+    regions = fieldnames(parench.(subs{ii}));
+    for j = 1:numel(regions)
+        % Iterate radii
+        radii = fieldnames(parench.(subs{ii}).(regions{j}));
+        for k = 1:numel(radii)
+            % Assign local variables
+            sub = subs{ii};
+            reg = regions{j};
+            rad = radii{k};
+            % Vessels
+            parench = rm_struct(parench, sub, reg, rad, 'ves',...
+                                max_pmus, max_pret);
+            % EPVS
+            parench = rm_struct(parench, sub, reg, rad, 'epvs',...
+                                max_pmus, max_pret);
+        end
+    end
+end
+
+%% Save as .MAT
+fout = fullfile(data_dir, fout);
+save(fout,"parench",'-mat','-v7.3');
+
+%% Function to identify outliers, remove, add back to struct
+
+function parench = rm_struct(parench, sub, reg, rad, loc, max_pmus, max_pret)
+% REMOVE_FROM_STRUCT
+% parench (struct): top-level structure
+% sub (string): subject
+% reg (string): region
+% rad (string): radius
+% loc (string): ves or epvs
+% max_pmus (int): maximum scattering coefficient
+% max_pret (int): maximum retardance
+
+% Select mus and retardance for this one
+mus = parench.(sub).(reg).(rad).outter.(loc).pmus;
+ret = parench.(sub).(reg).(rad).outter.(loc).pret;
+
+% Remove outliers based on defined thresholds
+mus_keep = mus < max_pmus;
+ret_keep = ret < max_pret;
+
+% Store cleaned data back into the structure
+parench.(sub).(reg).(rad).outter.(loc).pmus = mus(mus_keep);
+parench.(sub).(reg).(rad).outter.(loc).pret = ret(ret_keep);
+
+% Print the number of removed outliers for each location
+fprintf('Subject: %s, Region: %s, Radius: %s, Location: %s - Removed %d outliers from mus and %d from retardance.\n', ...
+    sub, reg, rad, loc, sum(~mus_keep), sum(~ret_keep));
+
 end
